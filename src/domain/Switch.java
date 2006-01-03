cd3ce0eec07fe0b9a459c32ec8d5b4956906168f
akula <akula@19f9ef3e-3ff7-0310-85f3-d10c0d820452>
2006-01-03 20:51:15 +0000

    moved around

---
+++ src/domain/Switch.java
@@ -0,0 +1,32 @@
+/*------------------------------------------------------------------------
+ * 
+ * <Switch.java>  -  <Represents a switch>
+ * 
+ * begin     : Tue Jan  3 21:24:40 CET 2006
+ * copyright : (C) by Benjamin Mueller 
+ * email     : bm@fork.ch
+ * language  : java
+ * version   : $Id$
+ * 
+ *----------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------------
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ *----------------------------------------------------------------------*/
+
+package ch.fork.RailControl.domain;
+
+public abstract class Switch {
+
+    private String name;
+    private String description;
+    private int address;
+
+    protected abstract void toggle();
+
+}
---
+++ src/domain/ThreeWaySwitch.java
@@ -0,0 +1,31 @@
+/*------------------------------------------------------------------------
+ * 
+ * <DefaultSwitch.java>  -  <A standard switch>
+ * 
+ * begin     : j Tue Jan  3 21:26:08 CET 2006
+ * copyright : (C) by Benjamin Mueller 
+ * email     : bm@fork.ch
+ * language  : java
+ * version   : $Id$
+ * 
+ *----------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------------
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ *----------------------------------------------------------------------*/
+
+package ch.fork.RailControl.domain;
+
+public class ThreeWaySwitch extends Switch {
+
+	protected void toggle() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
