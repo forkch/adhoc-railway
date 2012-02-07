918e146c362b625eb0ec8a7225afce013d23c3f5
fork_ch <fork_ch@19f9ef3e-3ff7-0310-85f3-d10c0d820452>
2012-02-07 12:54:33 +0000

    added Android stub

---
+++ AdHoc-Railway Android/src/ch/fork/adhocrailway/android/AdHocRailwayAndroidActivity.java
@@ -0,0 +1,17 @@
+package ch.fork.adhocrailway.android;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class AdHocRailwayAndroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.srcpd);
+        
+        Toast.makeText(this, "HELLO", Toast.LENGTH_LONG);
+    }
+}
