589f6d5bd5577e9db559da9f0639d684e0bf22e6
fork_ch <fork_ch@19f9ef3e-3ff7-0310-85f3-d10c0d820452>
2013-05-11 12:53:59 +0000

    - removed unused directories - reorganized test packages

--- src/ch/fork/AdHocRailway/services/impl/socketio/ServiceListener.java
+++
@@ -1,9 +0,0 @@
-package ch.fork.AdHocRailway.services.impl.socketio;
-
-public interface ServiceListener {
-	public void connected();
-
-	public void connectionError(Exception ex);
-
-	public void disconnected();
-}
