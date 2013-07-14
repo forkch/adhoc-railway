45b5094dabd20dff0949415db2c1a8da143230de
fork_ch <fork_ch@19f9ef3e-3ff7-0310-85f3-d10c0d820452>
2013-07-14 15:24:50 +0000

    gradelised AdHoc-Railway-Domain

---
+++ AdHoc-Railway-Domain/src/main/java/ch/fork/AdHocRailway/controllers/impl/srcp/SRCPTurnoutControlAdapter.java
@@ -0,0 +1,261 @@
+package ch.fork.AdHocRailway.controllers.impl.srcp;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+import ch.fork.AdHocRailway.controllers.TurnoutController;
+import ch.fork.AdHocRailway.domain.turnouts.Turnout;
+import ch.fork.AdHocRailway.domain.turnouts.TurnoutState;
+import ch.fork.AdHocRailway.manager.turnouts.TurnoutException;
+import de.dermoba.srcp.client.SRCPSession;
+import de.dermoba.srcp.model.SRCPModelException;
+import de.dermoba.srcp.model.turnouts.MMTurnout;
+import de.dermoba.srcp.model.turnouts.SRCPTurnout;
+import de.dermoba.srcp.model.turnouts.SRCPTurnoutChangeListener;
+import de.dermoba.srcp.model.turnouts.SRCPTurnoutControl;
+import de.dermoba.srcp.model.turnouts.SRCPTurnoutState;
+import de.dermoba.srcp.model.turnouts.SRCPTurnoutTypes;
+
+public class SRCPTurnoutControlAdapter extends TurnoutController implements
+		SRCPTurnoutChangeListener {
+	private static Logger logger = Logger
+			.getLogger(SRCPTurnoutControlAdapter.class);
+
+	private final Map<Turnout, SRCPTurnout> turnoutsSRCPTurnoutsMap = new HashMap<Turnout, SRCPTurnout>();
+	private final Map<SRCPTurnout, Turnout> SRCPTurnoutsTurnoutsMap = new HashMap<SRCPTurnout, Turnout>();
+
+	SRCPTurnoutControl turnoutControl;
+
+	SRCPTurnout sTurnoutTemp;
+
+	Turnout turnoutTemp;
+
+	public SRCPTurnoutControlAdapter() {
+
+		turnoutControl = SRCPTurnoutControl.getInstance();
+
+		turnoutControl.addTurnoutChangeListener(this);
+		reloadConfiguration();
+	}
+
+	@Override
+	public void setCurvedLeft(final Turnout turnout) throws TurnoutException {
+		final SRCPTurnout sTurnout = turnoutsSRCPTurnoutsMap.get(turnout);
+		try {
+			turnoutControl.setCurvedLeft(sTurnout);
+		} catch (final SRCPModelException e) {
+			throw new TurnoutException("Turnout Error", e);
+		}
+	}
+
+	@Override
+	public void setCurvedRight(final Turnout turnout) throws TurnoutException {
+		final SRCPTurnout sTurnout = turnoutsSRCPTurnoutsMap.get(turnout);
+		try {
+			turnoutControl.setCurvedRight(sTurnout);
+		} catch (final SRCPModelException e) {
+			throw new TurnoutException("Turnout Error", e);
+		}
+
+	}
+
+	@Override
+	public void setDefaultState(final Turnout turnout) throws TurnoutException {
+		final SRCPTurnout sTurnout = turnoutsSRCPTurnoutsMap.get(turnout);
+		try {
+			turnoutControl.setDefaultState(sTurnout);
+		} catch (final SRCPModelException e) {
+			throw new TurnoutException("Turnout Error", e);
+		}
+
+	}
+
+	@Override
+	public void setNonDefaultState(final Turnout turnout)
+			throws TurnoutException {
+		final SRCPTurnout sTurnout = turnoutsSRCPTurnoutsMap.get(turnout);
+		try {
+			turnoutControl.setNonDefaultState(sTurnout);
+		} catch (final SRCPModelException e) {
+			throw new TurnoutException("Turnout Error", e);
+		}
+
+	}
+
+	@Override
+	public void setStraight(final Turnout turnout) throws TurnoutException {
+		final SRCPTurnout sTurnout = turnoutsSRCPTurnoutsMap.get(turnout);
+		try {
+			turnoutControl.setStraight(sTurnout);
+		} catch (final SRCPModelException e) {
+			throw new TurnoutException("Turnout Error", e);
+		}
+
+	}
+
+	@Override
+	public void toggleTest(final Turnout turnout) throws TurnoutException {
+
+		turnoutControl.removeTurnout(sTurnoutTemp);
+		if (turnoutTemp == null || !turnoutTemp.equals(turnout)) {
+			if (sTurnoutTemp != null) {
+				turnoutControl.removeTurnout(sTurnoutTemp);
+			}
+			turnoutTemp = turnout;
+			// just create a temporary SRCPTurnout
+			sTurnoutTemp = createSRCPTurnout(turnout);
+		} else {
+			applyNewSettings(turnout);
+			sTurnoutTemp.setInitialized(false);
+		}
+		turnoutControl.addTurnout(sTurnoutTemp);
+		try {
+			turnoutControl.toggle(sTurnoutTemp);
+		} catch (final SRCPModelException e) {
+			throw new TurnoutException("Turnout Error", e);
+		}
+	}
+
+	@Override
+	public void toggle(final Turnout turnout) throws TurnoutException {
+		final SRCPTurnout sTurnout = turnoutsSRCPTurnoutsMap.get(turnout);
+
+		try {
+			turnoutControl.toggle(sTurnout);
+		} catch (final SRCPModelException e) {
+			throw new TurnoutException("Turnout Error", e);
+		}
+
+	}
+
+	@Override
+	public void reloadConfiguration() {
+	}
+
+	@Override
+	public void addOrUpdateTurnout(final Turnout turnout) {
+		turnoutControl.removeTurnout(getSRCPTurnout(turnout));
+		final SRCPTurnout sTurnout = createSRCPTurnout(turnout);
+
+		turnoutsSRCPTurnoutsMap.put(turnout, sTurnout);
+		SRCPTurnoutsTurnoutsMap.put(sTurnout, turnout);
+		turnoutControl.addTurnout(sTurnout);
+	}
+
+	@Override
+	public void turnoutChanged(final SRCPTurnout changedTurnout,
+			final SRCPTurnoutState newState) {
+		informListeners(changedTurnout);
+	}
+
+	public void setSession(final SRCPSession session) {
+		turnoutControl.setSession(session);
+	}
+
+	public SRCPTurnout getSRCPTurnout(final Turnout turnout) {
+		return turnoutsSRCPTurnoutsMap.get(turnout);
+	}
+
+	private void applyNewSettings(final Turnout turnout) {
+		sTurnoutTemp.setBus1(turnout.getBus1());
+		sTurnoutTemp.setBus2(turnout.getBus2());
+		sTurnoutTemp.setAddress1(turnout.getAddress1());
+		sTurnoutTemp.setAddress2(turnout.getAddress2());
+		sTurnoutTemp.setAddress1Switched(turnout.isAddress1Switched());
+		sTurnoutTemp.setAddress2Switched(turnout.isAddress2Switched());
+		setSRCPTurnoutDefaultState(sTurnoutTemp, turnout);
+		setSRCPTurnoutType(turnout, sTurnoutTemp);
+	}
+
+	private SRCPTurnout createSRCPTurnout(final Turnout turnout) {
+		final SRCPTurnout sTurnout = new MMTurnout();
+		sTurnout.setBus1(turnout.getBus1());
+		sTurnout.setBus2(turnout.getBus2());
+
+		sTurnout.setAddress1(turnout.getAddress1());
+		sTurnout.setAddress2(turnout.getAddress2());
+
+		sTurnout.setAddress1Switched(turnout.isAddress1Switched());
+		sTurnout.setAddress2Switched(turnout.isAddress2Switched());
+
+		setSRCPTurnoutDefaultState(sTurnout, turnout);
+
+		setSRCPTurnoutType(turnout, sTurnout);
+		return sTurnout;
+	}
+
+	private void setSRCPTurnoutType(final Turnout turnout,
+			final SRCPTurnout sTurnout) {
+		switch (turnout.getTurnoutType()) {
+		case DEFAULT_LEFT:
+		case DEFAULT_RIGHT:
+			sTurnout.setTurnoutType(SRCPTurnoutTypes.DEFAULT);
+			break;
+		case DOUBLECROSS:
+			sTurnout.setTurnoutType(SRCPTurnoutTypes.DOUBLECROSS);
+			break;
+		case CUTTER:
+			sTurnout.setTurnoutType(SRCPTurnoutTypes.CUTTER);
+			break;
+		case THREEWAY:
+			sTurnout.setTurnoutType(SRCPTurnoutTypes.THREEWAY);
+			break;
+		default:
+			sTurnout.setTurnoutType(SRCPTurnoutTypes.DEFAULT);
+		}
+	}
+
+	private void setSRCPTurnoutDefaultState(final SRCPTurnout sTurnout,
+			final Turnout turnout) {
+		switch (turnout.getDefaultState()) {
+		case STRAIGHT:
+			sTurnout.setDefaultState(SRCPTurnoutState.STRAIGHT);
+			break;
+		case LEFT:
+			sTurnout.setDefaultState(SRCPTurnoutState.LEFT);
+			break;
+		case RIGHT:
+			sTurnout.setDefaultState(SRCPTurnoutState.RIGHT);
+			break;
+		case UNDEF:
+			sTurnout.setDefaultState(SRCPTurnoutState.UNDEF);
+			break;
+		}
+	}
+
+	private void informListeners(final SRCPTurnout changedTurnout) {
+		// List<TurnoutChangeListener> ll = listeners.get(changedTurnout);
+		// if (ll == null)
+		// its a sub-turnout of a threeway turnout
+		// return;
+
+		Turnout turnout = SRCPTurnoutsTurnoutsMap.get(changedTurnout);
+		if (turnout == null) {
+			turnout = turnoutTemp;
+		}
+
+		informListeners(turnout,
+				getTurnoutStateFromSRCPTurnoutState(changedTurnout
+						.getTurnoutState()));
+		logger.debug("turnoutChanged(" + changedTurnout + ")");
+
+	}
+
+	private TurnoutState getTurnoutStateFromSRCPTurnoutState(
+			final SRCPTurnoutState turnoutState) {
+		switch (turnoutState) {
+		case LEFT:
+			return TurnoutState.LEFT;
+		case RIGHT:
+			return TurnoutState.RIGHT;
+		case STRAIGHT:
+
+			return TurnoutState.STRAIGHT;
+		case UNDEF:
+		default:
+			return TurnoutState.UNDEF;
+		}
+	}
+}
---
+++ AdHoc-Railway-Domain/src/main/java/ch/fork/AdHocRailway/services/impl/xml/XMLServiceHelper.java
@@ -0,0 +1,212 @@
+package ch.fork.AdHocRailway.services.impl.xml;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.SortedSet;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
+
+import ch.fork.AdHocRailway.domain.AbstractItem;
+import ch.fork.AdHocRailway.domain.locomotives.Locomotive;
+import ch.fork.AdHocRailway.domain.locomotives.LocomotiveFunction;
+import ch.fork.AdHocRailway.domain.locomotives.LocomotiveGroup;
+import ch.fork.AdHocRailway.domain.turnouts.Route;
+import ch.fork.AdHocRailway.domain.turnouts.RouteGroup;
+import ch.fork.AdHocRailway.domain.turnouts.RouteItem;
+import ch.fork.AdHocRailway.domain.turnouts.Turnout;
+import ch.fork.AdHocRailway.domain.turnouts.TurnoutGroup;
+import ch.fork.AdHocRailway.manager.locomotives.LocomotiveManager;
+import ch.fork.AdHocRailway.manager.turnouts.RouteManager;
+import ch.fork.AdHocRailway.manager.turnouts.TurnoutManager;
+
+import com.thoughtworks.xstream.XStream;
+
+public class XMLServiceHelper {
+
+	private static final Logger LOGGER = Logger
+			.getLogger(XMLServiceHelper.class);
+	private static final AtomicInteger counter = new AtomicInteger();
+
+	public static int nextValue() {
+		return counter.getAndIncrement();
+	}
+
+	public XMLServiceHelper() {
+	}
+
+	public void loadFile(final XMLLocomotiveService locomotiveService,
+			final XMLTurnoutService turnoutService,
+			final XMLRouteService routeService, final File xmlFile) {
+
+		AdHocRailwayData data;
+		try {
+			LOGGER.info("start loading locomotives, turnout and routes  from file: "
+					+ xmlFile);
+			final XStream xstream = getXStream();
+
+			data = (AdHocRailwayData) xstream.fromXML(new FileReader(xmlFile));
+			addFunctionsIfNeccesaray(data);
+
+			locomotiveService.loadLocomotiveGroupsFromXML(data
+					.getLocomotiveGroups());
+			turnoutService.loadTurnoutGroupsFromXML(data.getTurnoutGroups());
+			routeService.loadRouteGroupsFromXML(data.getRouteGroups());
+
+			LOGGER.info("finished loading locomotives, turnout and routes  from file: "
+					+ xmlFile);
+		} catch (final FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public void saveFile(final LocomotiveManager locomotiveManager,
+			final TurnoutManager turnoutManager,
+			final RouteManager routeManager, final File xmlFile)
+			throws IOException {
+
+		LOGGER.info("start saving/exporting locomotives, turnout and routes to file: "
+				+ xmlFile);
+		final SortedSet<LocomotiveGroup> locomotiveGroups = locomotiveManager
+				.getAllLocomotiveGroups();
+
+		final SortedSet<TurnoutGroup> turnoutGroups = turnoutManager
+				.getAllTurnoutGroups();
+
+		final SortedSet<RouteGroup> routeGroups = routeManager
+				.getAllRouteGroups();
+
+		final AdHocRailwayData data = new AdHocRailwayData(locomotiveGroups,
+				turnoutGroups, routeGroups);
+		final XStream xstream = getXStream();
+
+		final String xml = xstream.toXML(data);
+		FileUtils.writeStringToFile(xmlFile, xml);
+
+		LOGGER.info("finished saving locomotives, turnout and routes  to file: "
+				+ xmlFile);
+
+	}
+
+	public void exportLocomotivesToFile(final File fileToExport,
+			final LocomotiveManager locomotivePersistence) throws IOException {
+		LOGGER.info("start exporting locomotives to file: " + fileToExport);
+
+		final AdHocRailwayData data = new AdHocRailwayData(
+				locomotivePersistence.getAllLocomotiveGroups(), null, null);
+		final XStream xstream = getXStream();
+		final String xml = xstream.toXML(data);
+		FileUtils.writeStringToFile(fileToExport, xml);
+		LOGGER.info("finished exporting locomotives to file: " + fileToExport);
+	}
+
+	public void importLocomotivesFromFile(final File fileToImport,
+			final LocomotiveManager locomotivePersistence) {
+		AdHocRailwayData data;
+		try {
+			LOGGER.info("start importing locomotives from file: "
+					+ fileToImport);
+
+			final XStream xstream = getXStream();
+			data = (AdHocRailwayData) xstream.fromXML(new FileReader(
+					fileToImport));
+
+			addFunctionsIfNeccesaray(data);
+			new LocomotiveImporter().importLocomotives(locomotivePersistence,
+					data.getLocomotiveGroups());
+			LOGGER.info("finished importing locomotives from file: "
+					+ fileToImport);
+		} catch (final FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void addFunctionsIfNeccesaray(final AdHocRailwayData data) {
+		for (final LocomotiveGroup group : data.getLocomotiveGroups()) {
+			if (group.getLocomotives() == null) {
+				continue;
+			}
+			for (final Locomotive locomotive : group.getLocomotives()) {
+				if (locomotive.getFunctions() == null) {
+
+					switch (locomotive.getType()) {
+					case DELTA:
+						locomotive.setFunctions(LocomotiveFunction
+								.getDeltaFunctions());
+						break;
+					case DIGITAL:
+						locomotive.setFunctions(LocomotiveFunction
+								.getDigitalFunctions());
+						break;
+					case SIMULATED_MFX:
+						locomotive.setFunctions(LocomotiveFunction
+								.getSimulatedMfxFunctions());
+						break;
+					default:
+						break;
+
+					}
+
+				}
+			}
+		}
+	}
+
+	private XStream getXStream() {
+		final XStream xstream = new XStream();
+		xstream.omitField(Locomotive.class, "locomotiveGroup");
+		xstream.omitField(Turnout.class, "turnoutGroup");
+		xstream.omitField(Route.class, "routeGroup");
+		xstream.omitField(RouteItem.class, "route");
+
+		xstream.alias("AdHocRailwayData", AdHocRailwayData.class);
+		xstream.alias("locomotive", Locomotive.class);
+		xstream.alias("locomotiveGroup", LocomotiveGroup.class);
+		xstream.alias("turnout", Turnout.class);
+		xstream.alias("turnoutGroup", TurnoutGroup.class);
+		xstream.alias("route", Route.class);
+		xstream.alias("routeGroup", RouteGroup.class);
+		xstream.alias("routeItem", RouteItem.class);
+
+		xstream.useAttributeFor(LocomotiveGroup.class,
+				LocomotiveGroup.PROPERTYNAME_ID);
+		xstream.useAttributeFor(LocomotiveGroup.class,
+				LocomotiveGroup.PROPERTYNAME_NAME);
+		xstream.useAttributeFor(Locomotive.class, Locomotive.PROPERTYNAME_ID);
+		xstream.useAttributeFor(Locomotive.class, Locomotive.PROPERTYNAME_NAME);
+		xstream.useAttributeFor(Locomotive.class,
+				Locomotive.PROPERTYNAME_DESCRIPTION);
+		xstream.useAttributeFor(Locomotive.class, Locomotive.PROPERTYNAME_IMAGE);
+		xstream.useAttributeFor(Locomotive.class, Locomotive.PROPERTYNAME_BUS);
+		xstream.useAttributeFor(Locomotive.class,
+				Locomotive.PROPERTYNAME_ADDRESS1);
+		xstream.useAttributeFor(Locomotive.class,
+				Locomotive.PROPERTYNAME_ADDRESS2);
+		xstream.useAttributeFor(Locomotive.class,
+				Locomotive.PROPERTYNAME_LOCOMOTIVE_TYPE);
+
+		xstream.omitField(Locomotive.class, "changeSupport");
+		xstream.omitField(LocomotiveGroup.class, "changeSupport");
+		xstream.omitField(Turnout.class, "changeSupport");
+		xstream.omitField(TurnoutGroup.class, "changeSupport");
+		xstream.omitField(Route.class, "changeSupport");
+		xstream.omitField(RouteGroup.class, "changeSupport");
+		xstream.omitField(RouteItem.class, "changeSupport");
+		xstream.omitField(AbstractItem.class, "changeSupport");
+
+		xstream.addImplicitCollection(LocomotiveGroup.class, "locomotives");
+		xstream.addImplicitCollection(TurnoutGroup.class, "turnouts");
+		xstream.addImplicitCollection(RouteGroup.class, "routes");
+		xstream.addImplicitCollection(Route.class, "routeItems");
+
+		xstream.registerConverter(new LocomotiveTypeConverter());
+
+		return xstream;
+	}
+}
