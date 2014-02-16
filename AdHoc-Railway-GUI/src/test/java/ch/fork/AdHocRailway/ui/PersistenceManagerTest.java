d45dbdb81938f6abab0c7b2cec1967bba56f21ed
fork_ch <fork_ch@19f9ef3e-3ff7-0310-85f3-d10c0d820452>
2014-02-16 16:05:56 +0000

    persitence manager test

--- AdHoc-Railway-GUI/src/test/java/ch/fork/AdHocRailway/ui/PersistenceManagerTest.java
+++ AdHoc-Railway-GUI/src/test/java/ch/fork/AdHocRailway/ui/PersistenceManagerTest.java
@@ -1,7 +1,16 @@
 package ch.fork.AdHocRailway.ui;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+import ch.fork.AdHocRailway.services.impl.socketio.locomotives.SIOLocomotiveService;
+import ch.fork.AdHocRailway.services.impl.socketio.turnouts.SIORouteService;
+import ch.fork.AdHocRailway.services.impl.socketio.turnouts.SIOTurnoutService;
+import ch.fork.AdHocRailway.services.impl.xml.XMLLocomotiveService;
+import ch.fork.AdHocRailway.services.impl.xml.XMLRouteService;
+import ch.fork.AdHocRailway.services.impl.xml.XMLTurnoutService;
+import ch.fork.AdHocRailway.technical.configuration.Preferences;
+import ch.fork.AdHocRailway.technical.configuration.PreferencesKeys;
 import ch.fork.AdHocRailway.ui.context.ApplicationContext;
 import ch.fork.AdHocRailway.ui.context.PersistenceManagerContext;
 
@@ -10,18 +19,65 @@ public class PersistenceManagerTest {
 	private PersistenceManager testee;
 
 	@Test
-	public void loading_file() {
+	public void loadingPersistenceForFileMode() {
 		// given
-		testee = createTestee();
+		final PersistenceManagerContext applicationContext = createApplicationContextForFileLoading();
+		testee = createTestee(applicationContext);
+
+		// when
+		whenLoadingPersistenceLayer();
+
+		// then
+		Assert.assertTrue(applicationContext.getLocomotiveManager()
+				.getService() instanceof XMLLocomotiveService);
+		Assert.assertTrue(applicationContext.getTurnoutManager().getService() instanceof XMLTurnoutService);
+		Assert.assertTrue(applicationContext.getRouteManager().getService() instanceof XMLRouteService);
 	}
 
-	private PersistenceManager createTestee() {
-		return new PersistenceManager(createApplicationContextForFileLoading());
+	@Test
+	public void loadingPersistenceForAdHocServerMode() {
+		// given
+		final PersistenceManagerContext applicationContext = createApplicationContextForAdHocServer();
+		testee = createTestee(applicationContext);
+
+		// when
+		whenLoadingPersistenceLayer();
+
+		// then
+		Assert.assertTrue(applicationContext.getLocomotiveManager()
+				.getService() instanceof SIOLocomotiveService);
+		Assert.assertTrue(applicationContext.getTurnoutManager().getService() instanceof SIOTurnoutService);
+		Assert.assertTrue(applicationContext.getRouteManager().getService() instanceof SIORouteService);
+	}
+
+	private PersistenceManagerContext createApplicationContextForAdHocServer() {
+		final ApplicationContext applicationContext = new ApplicationContext();
+		final Preferences preferences = Preferences.getInstance();
+		preferences.setBooleanValue(PreferencesKeys.USE_ADHOC_SERVER, true);
+
+		applicationContext.setPreferences(preferences);
+		return applicationContext;
+	}
+
+	private void whenLoadingPersistenceLayer() {
+		testee.loadPersistenceLayer();
+	}
+
+	private void whenLoadingFile(final String string) {
+
+	}
+
+	private PersistenceManager createTestee(
+			final PersistenceManagerContext persistenceManagerContext) {
+		return new PersistenceManager(persistenceManagerContext);
 	}
 
 	private PersistenceManagerContext createApplicationContextForFileLoading() {
 		final ApplicationContext applicationContext = new ApplicationContext();
-		return applicationContext;
+		final Preferences preferences = Preferences.getInstance();
+		preferences.setBooleanValue(PreferencesKeys.USE_ADHOC_SERVER, false);
 
+		applicationContext.setPreferences(preferences);
+		return applicationContext;
 	}
 }
