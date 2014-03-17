966be5aaf93d641a9f6a0ad291aaed3dcd7c5bcb
fork_ch <fork_ch@19f9ef3e-3ff7-0310-85f3-d10c0d820452>
2014-03-17 21:46:27 +0000

    updated Master file

--- AdHoc-Railway-Web/publish_and_run.sh
+++ AdHoc-Railway-Web/publish_and_run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-scp -r app.js package.json models public routes views  baehnle@192.168.2.100:~/AdHoc-Railway/AdHoc-Railway-Web
+scp -r app.js package.json models public routes views  baehnle@192.168.11.10:~/AdHoc-Railway/AdHoc-Railway-Web
 
-ssh  baehnle@192.168.2.100
+ssh  baehnle@192.168.11.10
 
