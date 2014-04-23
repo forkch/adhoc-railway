078c7da6b642f355d324a91fde347003dd93cc5d
Benjamin Müller <bm@fork.ch>
2014-04-23 20:41:45 +0200

    updated deploy scripts

---
+++ tools/deploy_client_to_raspberry_pi.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ssh baehnle@adhocserver 'rm -rf /var/www/adhoc-railway/*'
+ssh baehnle@adhocserver 'rm -rf ~/AdHoc-Railway/ch.fork.adhocrailway.ui*'
+scp -r ch.fork.adhocrailway.ui/build/webstart/signed/* baehnle@adhocserver:/var/www/adhoc-railway
+scp -r ch.fork.adhocrailway.ui/build/distributions/* baehnle@adhocserver:~/AdHoc-Railway
+
---
+++ tools/deploy_servers_to_raspberry_pi.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+ssh -t baehnle@adhocserver 'sudo killall node'
+ssh baehnle@adhocserver 'rm -rf ~/AdHoc-Railway/AdHoc-Railway-Server'
+
+rm AdHoc-Railway-Server.tar.gz
+tar cvvf AdHoc-Railway-Server.tar AdHoc-Railway-Server
+gzip AdHoc-Railway-Server.tar
+
+scp -r AdHoc-Railway-Server.tar.gz baehnle@adhocserver:~/AdHoc-Railway
+
+ssh baehnle@adhocserver 'cd ~/AdHoc-Railway && tar xvf AdHoc-Railway-Server.tar.gz'
+ssh baehnle@adhocserver 'rm ~/AdHoc-Railway/AdHoc-Railway-Server.tar.gz'
+
+ssh baehnle@adhocserver 'rm -rf ~/AdHoc-Railway/AdHoc-Railway-Webclient'
+
+rm AdHoc-Railway-Webclient.tar.gz
+tar cvvf AdHoc-Railway-Webclient.tar AdHoc-Railway-Webclient 
+gzip AdHoc-Railway-Webclient.tar
+
+scp -r AdHoc-Railway-Webclient.tar.gz baehnle@adhocserver:~/AdHoc-Railway
+
+ssh baehnle@adhocserver 'cd ~/AdHoc-Railway && tar xvf AdHoc-Railway-Webclient.tar.gz'
+ssh baehnle@adhocserver 'rm ~/AdHoc-Railway/AdHoc-Railway-Webclient.tar.gz'
+
+ssh -t baehnle@adhocserver 'sudo service adhoc-railway-server start'
+ssh -t baehnle@adhocserver 'sudo service adhoc-railway-webclient start'
