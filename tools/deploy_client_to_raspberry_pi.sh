4762f95279fbe3fe98f30ae84ef3c9b8c36867ba
Benjamin Müller <bm@fork.ch>
2014-04-26 12:34:12 +0200

    created deployment page

--- tools/deploy_client_to_raspberry_pi.sh
+++ tools/deploy_client_to_raspberry_pi.sh
@@ -2,8 +2,9 @@
 
 gradle clean build distZip generateWebStartFiles
 
-ssh baehnle@adhocserver 'rm -rf /var/www/adhoc-railway/*'
+ssh baehnle@adhocserver 'rm -rf /var/www/adhoc-railway/artifacts/'
+ssh baehnle@adhocserver 'mkdir -p /var/www/adhoc-railway/artifacts'
 ssh baehnle@adhocserver 'rm -rf ~/AdHoc-Railway/ch.fork.adhocrailway.ui*'
-scp -r ch.fork.adhocrailway.ui/build/webstart/signed/* baehnle@adhocserver:/var/www/adhoc-railway
+scp -r ch.fork.adhocrailway.ui/build/webstart/signed/* baehnle@adhocserver:/var/www/adhoc-railway/artifacts
 scp -r ch.fork.adhocrailway.ui/build/distributions/* baehnle@adhocserver:~/AdHoc-Railway
 
