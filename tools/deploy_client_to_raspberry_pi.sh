875b19aaf4ad4d014b0c1f63118a67f685934fe6
Benjamin Müller <bm@fork.ch>
2014-04-26 12:00:01 +0200

    updated that it also builds

--- tools/deploy_client_to_raspberry_pi.sh
+++ tools/deploy_client_to_raspberry_pi.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+gradle clean build distZip generateWebStartFiles
+
 ssh baehnle@adhocserver 'rm -rf /var/www/adhoc-railway/*'
 ssh baehnle@adhocserver 'rm -rf ~/AdHoc-Railway/ch.fork.adhocrailway.ui*'
 scp -r ch.fork.adhocrailway.ui/build/webstart/signed/* baehnle@adhocserver:/var/www/adhoc-railway
