bf123ca5c5682b9060c2a4ac80125c8a524a3dd1
ben <ben@getbring.com>
2017-04-06 21:44:49 +0200

    just show the damn scrollbar...

--- ch.fork.adhocrailway.ui/src/main/java/ch/fork/AdHocRailway/ui/locomotives/LocomotiveControlPanel.java
+++ ch.fork.adhocrailway.ui/src/main/java/ch/fork/AdHocRailway/ui/locomotives/LocomotiveControlPanel.java
@@ -53,7 +53,7 @@ public class LocomotiveControlPanel extends JPanel {
     private void initGUI() {
         setLayout(new MigLayout("fill, insets 0"));
         controlPanel = new JPanel(new MigLayout("insets 0"));
-        JScrollPane controlPanelScrollPane = new JScrollPane(controlPanel, ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        JScrollPane controlPanelScrollPane = new JScrollPane(controlPanel, ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
 
         final SimpleInternalFrame locomotivesFrame = new SimpleInternalFrame(
                 "Trains");
