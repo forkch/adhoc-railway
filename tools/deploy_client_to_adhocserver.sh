993be3b50af08693e6df9fb95d3df0cb15621473
Benjamin Müller <ben@getbring.com>
2023-11-16 15:02:22 +0100

    updated gradle and deploy script

--- tools/deploy_client_to_adhocserver.sh
+++ tools/deploy_client_to_adhocserver.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-./gradlew --offline -x test clean build distZip generateWebStartFiles -x createDmg
+./gradlew --offline -x test clean build distZip shadowJar 
 
-ssh baehnle@adhocserver 'rm -rf /var/www/adhoc-railway/artifacts/'
-ssh baehnle@adhocserver 'mkdir -p /var/www/adhoc-railway/artifacts'
-ssh baehnle@adhocserver 'rm -rf ~/AdHoc-Railway/ch.fork.adhocrailway.ui*'
-scp -r ch.fork.adhocrailway.ui/build/webstart/signed/* baehnle@adhocserver:/var/www/adhoc-railway/artifacts
-scp -r ch.fork.adhocrailway.ui/build/distributions/* baehnle@adhocserver:~/AdHoc-Railway
+ssh baehnle@adhocserver 'rm -rf /var/www/adhoc-railway/artifacts/ && mkdir -p /var/www/adhoc-railway/artifacts && rm -rf ~/AdHoc-Railway/ch.fork.adhocrailway.ui*'
+#scp -r ch.fork.adhocrailway.ui/build/webstart/signed/* baehnle@adhocserver:/var/www/adhoc-railway/artifacts
+#scp -r ch.fork.adhocrailway.ui/build/distributions/* baehnle@adhocserver:~/AdHoc-Railway
+#scp -r ch.fork.adhocrailway.ui/build/distributions/* baehnle@adhocserver:/var/www/adhoc-railway/artifacts
+scp -r ch.fork.adhocrailway.ui/build/libs/adhocrailway-gui.jar baehnle@adhocserver:/var/www/adhoc-railway/artifacts
 
